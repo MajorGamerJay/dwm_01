1078c00a04da734a00f0827e124db1a1d4674b04
MajorGamerJay <majorgamerjay@protonmail.com>
2020-09-21 22:41:03 +0600

    Themed it to look like C64

--- config.def.h
+++ config.def.h
@@ -13,9 +13,9 @@ static const char *fonts[]          = { "Ubuntu Mono:style=regular:pixelsize=16"
 static const char dmenufont[]       = "Ubuntu Mono:style=regular:pixelsize=16";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray3[]       = "#000000";
+static const char col_gray4[]       = "#c6c6cd";
+static const char col_cyan[]        = "#4242c6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
